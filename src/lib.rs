c32c5ca45ea70da38e14016e51f95779f18ffb0d
Danny Milosavljevic <daym@oxidecomputer.com>
2021-06-08 01:32:14 +0200

    Clean up comments.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,6 @@
 #![feature(min_const_generics)]
 #![cfg_attr(not(feature = "std"), no_std)]
 
-//#![no_std]
-//#[cfg(feature="std")]
-//extern crate std;
-
 mod ondisk;
 mod image;
 pub use image::APCB;
