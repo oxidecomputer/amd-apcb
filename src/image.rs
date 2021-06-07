deafd94cc4cb4aba697ad6aea34257cd00d183c2
Danny Milosavljevic <daym@oxidecomputer.com>
2021-06-08 01:03:07 +0200

    image: Prepare for modifiable header.

--- src/image.rs
+++ src/image.rs
@@ -4,7 +4,7 @@ use crate::ondisk::APCB_V3_HEADER_EXT;
 use zerocopy::LayoutVerified;
 
 pub struct APCB<'a> {
-    header: APCB_V2_HEADER,
+    header: &'a APCB_V2_HEADER,
     v3_header_ext: Option<APCB_V3_HEADER_EXT>,
     beginning_of_groups: &'a mut [u8],
 }
@@ -18,14 +18,14 @@ type Result<Q> = core::result::Result<Q, Error>;
 
 impl<'a> APCB<'a> {
     pub fn load(backing_store: &'a mut [u8]) -> Result<Self> {
-        let (header, mut rest) = LayoutVerified::<_, APCB_V2_HEADER>::new_unaligned_from_prefix(backing_store).ok_or_else(|| Error::MarshalError)?;
+        let (header, mut rest) = LayoutVerified::<_, APCB_V2_HEADER>::new_unaligned_from_prefix(&mut *backing_store).ok_or_else(|| Error::MarshalError)?;
         let header = header.into_ref();
 
         assert!(usize::from(header.header_size) >= size_of::<APCB_V2_HEADER>());
         assert!(header.version.get() == 0x30);
         assert!(header.apcb_size.get() >= header.header_size.get().into());
 
-        let v3_header_ext = if usize::from(header.header_size) > size_of::<APCB_V2_HEADER>() {
+        let v3_header_ext = if usize::from(header.header_size) == size_of::<APCB_V2_HEADER>() + size_of::<APCB_V3_HEADER_EXT>() {
             let (value, r) = LayoutVerified::<_, APCB_V3_HEADER_EXT>::new_unaligned_from_prefix(rest).ok_or_else(|| Error::MarshalError)?;
             rest = r;
             let value = value.into_ref();
@@ -35,14 +35,14 @@ impl<'a> APCB<'a> {
             assert!(value.ext_header_size.get() == 88);
             assert!(u32::from(value.data_offset.get()) == value.ext_header_size.get());
             assert!(value.signature_ending == *b"BCPA");
-            //// TODO: Maybe skip weird header
             Some(*value)
         } else {
+            //// TODO: Maybe skip weird header
             None
         };
 
         Ok(Self {
-            header: *header,
+            header: header,
             v3_header_ext: v3_header_ext,
             beginning_of_groups: rest,
         })
