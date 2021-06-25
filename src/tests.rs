6f72191e845c098db997ce46d40606d70b1560a3
Danny Milosavljevic <daym@oxidecomputer.com>
2021-06-25 18:48:50 +0200

    tests: Add insert_two_tokens test.

--- src/tests.rs
+++ src/tests.rs
@@ -361,6 +361,89 @@ mod tests {
     }
 
     #[test]
+    fn insert_two_tokens() -> Result<(), Error> {
+        let mut buffer: [u8; 8 * 1024] = [0xFF; 8 * 1024];
+        let mut apcb = Apcb::create(&mut buffer[0..]).unwrap();
+        apcb.insert_group(0x1701, *b"PSPG")?;
+        apcb.insert_group(0x1704, *b"MEMG")?;
+        apcb.insert_group(0x3000, *b"TOKN")?;
+        //let mut apcb = Apcb::load(&mut buffer[0..]).unwrap();
+        apcb.insert_entry(0x1701, 96, 0, 0xFFFF, ContextType::Struct, &[1u8; 48], 33)?;
+        // makes it work let mut apcb = Apcb::load(&mut buffer[0..]).unwrap();
+        apcb.insert_entry(0x1701, 97, 0, 0xFFFF, ContextType::Struct, &[2u8; 1], 32)?;
+
+        // let mut apcb = Apcb::load(&mut buffer[0..]).unwrap();
+
+        // Insert empty "Token Entry"
+        // insert_entry(&mut self, group_id: u16, entry_id: u16, instance_id: u16, board_instance_mask: u16, context_type: ContextType, payload: &[u8], priority_mask: u8
+        apcb.insert_entry(0x3000, TokenType::Byte as u16, 0, 1, ContextType::Tokens, &[], 32)?;
+
+        let mut apcb = Apcb::load(&mut buffer[0..]).unwrap();
+
+        // pub(crate) fn insert_token(&mut self, group_id: u16, entry_id: u16, instance_id: u16, board_instance_mask: u16, token_id: u32, token_value: u32) -> Result<()> {
+        apcb.insert_token(0x3000, TokenType::Byte as u16, 0, 1, 0x014FBF20, 1)?;
+        apcb.insert_token(0x3000, TokenType::Byte as u16, 0, 1, 0x42, 2)?;
+
+        let apcb = Apcb::load(&mut buffer[0..]).unwrap();
+
+        let mut groups = apcb.groups();
+
+        let group = groups.next().ok_or_else(|| Error::GroupNotFound)?;
+        assert!(group.id() == 0x1701);
+        assert!(group.signature() ==*b"PSPG");
+
+        let mut entries = group.entries();
+
+        let entry = entries.next().ok_or_else(|| Error::EntryNotFound)?;
+        assert!(entry.id() == 96);
+        assert!(entry.instance_id() == 0);
+        assert!(entry.board_instance_mask() == 0xFFFF);
+
+        let entry = entries.next().ok_or_else(|| Error::EntryNotFound)?;
+        assert!(entry.id() == 97);
+        assert!(entry.instance_id() == 0);
+        assert!(entry.board_instance_mask() == 0xFFFF);
+
+        assert!(matches!(entries.next(), None));
+
+        let group = groups.next().ok_or_else(|| Error::GroupNotFound)?;
+        assert!(group.id() == 0x1704);
+        assert!(group.signature() ==*b"MEMG");
+        for _entry in group.entries() {
+            assert!(false);
+        }
+
+        let group = groups.next().ok_or_else(|| Error::GroupNotFound)?;
+        assert!(group.id() == 0x3000);
+        assert!(group.signature() ==*b"TOKN");
+        let mut entries = group.entries();
+
+        let entry = entries.next().ok_or_else(|| Error::EntryNotFound)?;
+        match entry.body {
+            EntryItemBody::<_>::Tokens(tokens) => {
+                let mut tokens = tokens.iter();
+
+                let token = tokens.next().ok_or_else(|| Error::TokenNotFound)?;
+                assert!(token.id() == 0x42);
+                assert!(token.value() == 2);
+
+                let token = tokens.next().ok_or_else(|| Error::TokenNotFound)?;
+                assert!(token.id() == 0x014FBF20);
+                assert!(token.value() == 1);
+
+                assert!(matches!(tokens.next(), None));
+            },
+            _ => {
+                panic!("unexpected entry type");
+            }
+        }
+        assert!(matches!(entries.next(), None));
+
+        assert!(matches!(groups.next(), None));
+        Ok(())
+    }
+
+    #[test]
     fn delete_tokens() -> Result<(), Error> {
         let mut buffer: [u8; 8 * 1024] = [0xFF; 8 * 1024];
         let mut apcb = Apcb::create(&mut buffer[0..]).unwrap();
