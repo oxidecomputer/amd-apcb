a64404a42cdc57f689413d9b00c4befba91138d2
Danny Milosavljevic <daym@oxidecomputer.com>
2021-09-24 23:13:39 +0200

    apcb: Apcb: Simplify internal_insert_entry.

--- src/apcb.rs
+++ src/apcb.rs
@@ -312,7 +312,7 @@ impl<'a> Apcb<'a> {
         self.group_mut(group_id).ok_or(Error::GroupNotFound)
     }
     #[pre]
-    fn internal_insert_entry(&mut self, entry_id: EntryId, instance_id: u16, board_instance_mask: u16, context_type: ContextType, payload_size: usize, priority_mask: PriorityLevels, payload_initializer: &mut dyn FnMut(&mut [u8]) -> ()) -> Result<()> {
+    fn internal_insert_entry(&mut self, entry_id: EntryId, instance_id: u16, board_instance_mask: u16, context_type: ContextType, payload_size: usize, priority_mask: PriorityLevels, payload_initializer: &mut dyn FnMut(&mut [u8])) -> Result<()> {
         let group_id = entry_id.group_id();
         let mut group = self.group_mut(group_id).ok_or(Error::GroupNotFound)?;
         match group.entry_mut(entry_id, instance_id, board_instance_mask) {
