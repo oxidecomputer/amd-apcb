74ab3962c605ef38c7aaa450594260932eb815fe
Danny Milosavljevic <daym@oxidecomputer.com>
2023-08-17 02:17:56 +0200

    Rename "SerdeApcb" to "Apcb" in serde
    Fixes <https://github.com/oxidecomputer/amd-apcb/issues/104>.

--- src/apcb.rs
+++ src/apcb.rs
@@ -64,6 +64,7 @@ pub struct Apcb<'a> {
 
 #[cfg(feature = "serde")]
 #[cfg_attr(feature = "serde", derive(Default, serde::Deserialize))]
+#[cfg_attr(feature = "serde", serde(rename = "Apcb"))]
 #[cfg_attr(feature = "serde", serde(deny_unknown_fields))]
 #[cfg_attr(feature = "schemars", derive(schemars::JsonSchema))]
 pub struct SerdeApcb {
